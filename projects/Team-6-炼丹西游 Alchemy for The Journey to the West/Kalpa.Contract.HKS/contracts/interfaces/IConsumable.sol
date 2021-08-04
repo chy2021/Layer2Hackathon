70de5698cd0a69c59a99c428d0a5c335d1d6a7ed
CHY <caooooohy@gmail.com>
2021-08-05 01:04:52 +0800

    Delete projects/Team-6-炼丹西游 Alchemy for The Journey to the West/Kalpa.Contract.HKS/contracts directory

--- projects/Team-6-炼丹西游 Alchemy for The Journey to the West/Kalpa.Contract.HKS/contracts/interfaces/IConsumable.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-/**
- * @dev Interface of the ACGNFT minter
- */
-interface IConsumable {
-    function consume(address account, uint256 amount) external;
-}
--- projects/Team-6-炼丹西游 Alchemy for The Journey to the West/Kalpa.Contract.HKS/contracts/interfaces/ISamsara.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-/**
- * @dev Interface of the ACGNFT minter
- */
-interface ISamsara {
-    function addRecycledToken(uint256 amount) external;
-}
